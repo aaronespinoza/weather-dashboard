08c26cfbe19d15afccff2ff89a0baabdfd3be3b1
aaronespinoza <aarone@berkeley.edu>
2021-12-09 16:25:11 -0800

    today card in row with search and fetch working

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,7 @@ function returnWeather() {
     //event.preventDefault()
     console.log("Doja")
     //var date= document.querySelector("#dateInput").value;
-   var cityInput= document.querySelector("#search").value;
+   var cityInput= document.querySelector("#soup").value;
    //converts cities with spaces in the name to have a plus instead for url
    //var date= "2021-12-05";
    //var city= "new+york"
@@ -18,9 +18,9 @@ function returnWeather() {
    var city= cityInput.split(' ').join('+');
    
    
-     var requestUrl = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+key;
+     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+key;
      //var requestUrl = "api.openweathermap.org/data/2.5/weather?q=atlanta&appid=88297b00647b4e5314bdcabb6ec68ee7"
-   
+    console.log(requestUrl);
      fetch(requestUrl)
        .then(function (response) {
          return response.json()
@@ -45,24 +45,40 @@ var todayHumidity=document.getElementById("todayHumidity");
 var todayUv=document.getElementById("todayUv");
 
 
-//PRINT Results function
+//PRINT Today's Results function
 function printResults(data){
 //erase previous results
-todayCity.innerHTML="";
-todayTemp.innerHTML="";
-todayWind.innerHTML="";
-todayHumidity.innerHTML="";
-todayUv.innerHTML="";
+todayCity.textContent="";
+todayTemp.textContent="";
+todayWind.textContent="";
+todayHumidity.textContent="";
+todayUv.textContent="";
 //Add text content
 todayCity.textContent= data.name+moment(data.dt.value).format("MMM D, YYYY");
-todayTemp.textContent = data.main.temp + " °F";
-todayWind.textContent = data.wind.speed + " MPH";
-todayHumidity.textContent= data.main.humidity + "%";
+todayTemp.textContent = "Temp:" +data.main.temp + " °F";
+todayWind.textContent = "Wind" +data.wind.speed + " MPH";
+todayHumidity.textContent= "Humidity" +data.main.humidity + "%";
 todayUv.textContent= data
 }
 // todayTemp
 // todayWind
 // todayHumidity
 // todayUv
+
+//PRINT 5 Day Forecast function
+function print5Day(data){
+    //erase previous results
+    todayCity.textContent="";
+    todayTemp.textContent="";
+    todayWind.textContent="";
+    todayHumidity.textContent="";
+    todayUv.textContent="";
+    //Add text content
+    todayCity.textContent= data.name+moment(data.dt.value).format("MMM D, YYYY");
+    todayTemp.textContent = "Temp:" +data.main.temp + " °F";
+    todayWind.textContent = "Wind" +data.wind.speed + " MPH";
+    todayHumidity.textContent= "Humidity" +data.main.humidity + "%";
+    todayUv.textContent= data
+    }
 
 
